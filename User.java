58f6b411e6ba270ed0f6b19b86c6c50a75799fe4
yunzhifeng01 <yunzhifeng01@126.com>
2020-09-18 08:22:33 +0800

    给User添加showUserName方法

--- User.java
+++ User.java
@@ -2,6 +2,10 @@ public class User{
 	private String  name;
 	private   int age;
 	private   String  gender;//添加性别属性
-        private   doubl   score;//远程终端修改
+    private   doubl   score;//远程终端修改
+	//添加方法
+	public  void showUserName(){
+		
+	}
 	
 }
